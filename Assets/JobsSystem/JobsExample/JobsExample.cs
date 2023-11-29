a1507418bb23cb048f95c83bd01ee9b61b39092b
Ivanov Evgenii <ivanjohnoff@gmail.com>
2023-11-29 08:36:49 +0300

    jobs examples lesson

---
+++ Assets/JobsSystem/JobsExample/JobsExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+
+namespace JobsSystem.JobsExample
+{
+    
+    public class JobsExample : MonoBehaviour
+    {
+        
+        private void Start()
+        {
+            NativeArray<int> array = new NativeArray<int>(10, Allocator.Temp);
+            array.Dispose();
+        }
+
+
+
+        private struct AdvancedJob : IJobParallelFor
+        {
+            public NativeArray<Vector3> Array;
+
+            public void Execute(int index)
+            {
+                
+            }
+        }
+
+
+        private struct SomeJob : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray<Vector3> Positions;
+
+            [WriteOnly]
+            public NativeArray<Vector3> NewPositions;
+
+            
+            public void Execute(int index)
+            {
+                NewPositions[index] = Positions[0];
+            }
+        }
+        
+        
+        private struct OtherJob : IJobParallelFor
+        {
+            [WriteOnly]
+            public NativeArray<Vector3> Positions;
+
+            [ReadOnly]
+            public NativeArray<Vector3> NewPositions;
+
+            
+            public void Execute(int index)
+            {
+                Positions[index] = NewPositions[index];
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/JobsSystem/JobsExample/OtherObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+
+namespace JobsSystem.JobsExample
+{
+    
+    public class OtherObject : MonoBehaviour
+    {
+
+        private NativeArray<Vector3> array;
+        private JobHandle handle;
+
+        
+        private void Start()
+        {
+            this.array = new NativeArray<Vector3>(100, Allocator.TempJob);
+            AdvancedJob job = new AdvancedJob();
+            job.Array = array;
+
+            handle = job.Schedule(100, 5);
+            handle.Complete();
+
+            StartCoroutine(JobCoroutine());
+        }
+
+        
+        private IEnumerator JobCoroutine()
+        {
+            while (handle.IsCompleted == false)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            foreach (var vector in array)
+            {
+                Debug.Log(vector);
+            }
+
+            array.Dispose();
+        }
+        
+        
+        public struct AdvancedJob : IJobParallelFor
+        {
+            public NativeArray<Vector3> Array;
+
+            public void Execute(int index)
+            {
+                Array[index] = Array[index].normalized;
+            }
+        }
+        
+        
+    }
+}
