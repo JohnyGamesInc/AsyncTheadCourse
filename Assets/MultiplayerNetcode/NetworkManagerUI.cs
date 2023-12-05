ed4f746d75e544a071ab01a4984e70bdb365f244
Ivanov Evgenii <ivanjohnoff@gmail.com>
2023-12-05 21:23:30 +0300

    manager ui

---
+++ Assets/MultiplayerNetcode/NetworkManagerUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MultiplayerNetcode
+{
+    
+    public class NetworkManagerUI : MonoBehaviour
+    {
+
+        [SerializeField] private Button serverButton;
+        [SerializeField] private Button hostButton;
+        [SerializeField] private Button clientButton;
+
+
+        private void Awake()
+        {
+            serverButton.onClick.AddListener(() =>
+            {
+                NetworkManager.Singleton.StartServer();
+            });
+            
+            hostButton.onClick.AddListener(() =>
+            {
+                NetworkManager.Singleton.StartHost();
+            });
+            
+            clientButton.onClick.AddListener(() =>
+            {
+                NetworkManager.Singleton.StartClient();
+            });
+        }
+        
+        
+    }
+}
---
+++ Assets/MultiplayerNetcode/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+
+namespace MultiplayerNetcode
+{
+    
+    public class Player : NetworkBehaviour
+    {
+
+        [SerializeField] private GameObject playerPrefab;
+        
+        private GameObject playerCharacter;
+
+        
+        
+        private void Start()
+        {
+            SpawnCharacter();
+        }
+
+
+        private void SpawnCharacter()
+        {
+            if (!IsServer) return;
+
+            playerCharacter = Instantiate(playerPrefab);
+            
+            // NetworkServer.SpawnWithClientAuthority(playerCharacter, connectionToClient);
+            // NetworkConfig.Prefabs.NetworkPrefabsLists
+        }
+        
+        
+    }
+}
